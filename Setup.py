6046a6d776549ec09c164bdfa5a5ac51becf492c
ParvathiRNair <124461471+ParvathiRNair@users.noreply.github.com>
2023-02-07 11:07:58 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,6 +4,6 @@ class wow:
     api_hash = "5cf3577d85fd02286535ec2296934287" #ypur API_HASH
     bot_token = "6068792377:AAGNincJ83esFClyJEDbtCctWhhDCalzgDM" #ypur bot_token
     openai_api_key = "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" #get this value from https://beta.openai.com/.
-    model = "text-davinci-002" # use any of these [text-davinci-002,text-davinci-001]
+    model = "text-davinci-003" # use any of these [text-davinci-002,text-davinci-001]
     mxtoken = 1024 #can decrese/increse with reaspect to result previlage
 #Note incresing the value of temp can cause increse usage of openai free tier so i prefer use this for your own usage
